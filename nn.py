e6b492275eb025a731842866546f93b194cba707
kanish26 <90209787+kanish26@users.noreply.github.com>
2023-10-07 19:18:48 +0530

    Trail Update 1

--- nn.py
+++ nn.py
@@ -1,2 +1,64 @@
+import numpy as np
 import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import SimpleRNN, Dense
+from xml.dom import minidom
 
+# Define a function to preprocess SVG data
+def preprocess_svg(svg_str, max_sequence_length):
+    # Extract the content within the <path> tag
+    path_data = minidom.parseString(svg_str).getElementsByTagName('path')[0].getAttribute('d')
+    
+    # Pad or truncate the path data to a fixed length
+    if len(path_data) < max_sequence_length:
+        path_data = path_data.ljust(max_sequence_length)
+    else:
+        path_data = path_data[:max_sequence_length]
+    
+    # Convert characters to integers using a simple mapping
+    char_to_int = {char: i for i, char in enumerate(set(path_data))}
+    int_to_char = {i: char for char, i in char_to_int.items()}
+    encoded_path = [char_to_int[char] for char in path_data]
+    
+    return encoded_path
+
+# Define a function to generate SVG from integer sequences
+def generate_svg(encoded_seq, int_to_char):
+    path_data = ''.join([int_to_char[i] for i in encoded_seq])
+    svg_xml = f'<svg><path d="{path_data}"/></svg>'
+    return svg_xml
+
+# Define parameters
+max_sequence_length = 100
+vocab_size = 256  # Number of unique characters in the SVG path data
+hidden_units = 64
+
+# Create the RNN model
+model = Sequential([
+    SimpleRNN(hidden_units, input_shape=(max_sequence_length, vocab_size), return_sequences=True),
+    Dense(vocab_size, activation='softmax')
+])
+
+# Compile the model
+model.compile(loss='categorical_crossentropy', optimizer='adam')
+
+# Generate random example data (you should replace this with real SVG data)
+input_svg = '<svg><path d="M10 10 L50 50 Z"/></svg>'
+target_svg = '<svg><path d="M50 50 L100 100 Z"/></svg>'
+
+# Preprocess the input and target SVGs
+encoded_input = preprocess_svg(input_svg, max_sequence_length)
+encoded_target = preprocess_svg(target_svg, max_sequence_length)
+
+# Convert encoded sequences to one-hot encoding
+X = tf.one_hot(encoded_input, vocab_size)
+y = tf.one_hot(encoded_target, vocab_size)
+
+# Train the model (you may need more data and epochs for better results)
+model.fit(np.array([X]), np.array([y]), epochs=10)
+
+# Generate SVG output
+predicted_output = model.predict(np.array([X]))
+generated_svg = generate_svg(np.argmax(predicted_output, axis=-1)[0], int_to_char)
+print("Generated SVG:")
+print(generated_svg)
